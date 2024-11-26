7c549a93aaf69082e3cadc535d1a12c896601731
Oliver Chong <oliverchong987@gmail.com>
2024-11-26 15:25:20 +0800

    WIP

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+interface User {
+    id: number;
+    name: string;
+}
 
-const UsersPage = () => {
+const UsersPage = async () => {
+    // const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    // const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: "no-cache"})
+
+    //re-request data after 10 seconds (no data cache)
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users: User[] = await res.json()
     return (
-        <div>UsersPage</div>
+        <>
+        <h1>Users</h1>
+        <p>{new Date().toLocaleTimeString()}</p>
+        <ul>
+            {
+                users.map(user => <li key = {user.id}>
+                    {user.name}
+                </li>)
+            }
+        </ul>
+        </>
     )
 }
 
