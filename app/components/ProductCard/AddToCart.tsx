662d689d11822432690a13cdc195fe131ccb273c
OLIVER CHONG <oliverchong987@gmail.com>
2024-11-27 14:22:40 +0800

    WIP

--- app/components/ProductCard/AddToCart.tsx
+++ app/components/ProductCard/AddToCart.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 const AddToCart = () => {
   return (
     <div>
-           <button onClick = {() => {
-            console.log("clck event")
-        }}>click event here</button>
+      <button onClick={() => {
+        console.log("clck event")
+      }} className='btn btn-primary'>Add to Cart</button>
     </div>
   )
 }
---
+++ app/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddToCart from 'components/ProductCard/AddToCart'
+// import styles from 'components/ProductCard/ProductCard.module.css'
+
+
+const ProductCard = () => {
+  return (
+    // <div className={styles.cardContainer}>
+    <div>
+      <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
